0458e1be050897550adfbb3ce2fa5c3988a1de26
Alhammadi Salem <alhammadi.30.40.50@gmail.com>
2022-12-22 17:06:29 +0300

    Update PizzaAdvice.java

--- Day 9/Day 9/Final Project/Final-Project/src/main/java/Pizza/Advice/PizzaAdvice.java
+++ Day 9/Day 9/Final Project/Final-Project/src/main/java/Pizza/Advice/PizzaAdvice.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class PizzaAdvice extends RuntimeException{
+public class PizzaAdvice{
     @ExceptionHandler(value = ApiException.class)
     public ResponseEntity ApiException (ApiException e){
         String message =e.getMessage();
